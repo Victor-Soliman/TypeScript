c8043d25163a50eb59484f68e7e864eb6f45c156
Nasr Soliman <victorsoliman7@gmail.com>
2023-07-16 21:57:29 +0300

    adding info

--- src/typeAnnotation.ts
+++ src/typeAnnotation.ts
@@ -1,40 +1,42 @@
+
+// pritives goes to stack in memory, while objects goes to heap 
 let theName = "Nasr";
 let theAge: number = 40;
 let hire: boolean = true;
-let all:any = "nana";
+let all: any = "nana";
 let allVars; //any
 
 
 theName = "Soliman";
 all = 100;
 
-function add(n1: number, n2:number){
-    return n1 + n2;
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-console.log(add(10,20));
-console.log(typeof add(10,20));
+console.log(add(10, 20));
+console.log(typeof add(10, 20));
 console.log("---------");
 // type annotation with array
-let everything : string | number | boolean = "Nasr";
+let everything: string | number | boolean = "Nasr";
 
 everything = "Nasr";
 everything = 10;
 everything = true;
 
-let myFriends: string[] = ["Nasr","Marius","Joe"]
-for(let i = 0 ; i < myFriends.length ; i++){
-    console.log(myFriends[i].repeat(3));
+let myFriends: string[] = ["Nasr", "Marius", "Joe"]
+for (let i = 0; i < myFriends.length; i++) {
+  console.log(myFriends[i].repeat(3));
 }
 
 console.log("---------");
 // type annotation with Multidimensional Arrays
 
-let arrayOne: number[] = [1,2,3,4,5];
-let arrayTwo: string[] = ["A","B","C"];
-let arrayThree: (string | number)[] = [1,2,3,4,"A","B","C"];
+let arrayOne: number[] = [1, 2, 3, 4, 5];
+let arrayTwo: string[] = ["A", "B", "C"];
+let arrayThree: (string | number)[] = [1, 2, 3, 4, "A", "B", "C"];
 
-let arrayFour: (string | number | string[] | boolean)[] = [1,2,3,4,"A","B", ["C","D"],true,false];
+let arrayFour: (string | number | string[] | boolean)[] = [1, 2, 3, 4, "A", "B", ["C", "D"], true, false];
 console.log(arrayFour);
 
 console.log("---------");
@@ -52,16 +54,16 @@ console.log("---------");
 
   - noUnusedParameters
   --- Report Errors On Unused Parameters In Functions.
-*/  
+*/
 let showMsg = true;
-function showDetails(name: string, age: number, salary: number):  string{
-    let test = 10;
-    if(showMsg){
-        return `Hello ${name}, Age Is ${age}, Salary Is ${salary}, Test Variable ${test}`
-    }
-    return `No Data to show`;
+function showDetails(name: string, age: number, salary: number): string {
+  let test = 10;
+  if (showMsg) {
+    return `Hello ${name}, Age Is ${age}, Salary Is ${salary}, Test Variable ${test}`
+  }
+  return `No Data to show`;
 }
-console.log(showDetails("Nasr",29, 5000));
+console.log(showDetails("Nasr", 29, 5000));
 
 console.log("---------");
 /*
@@ -71,11 +73,11 @@ console.log("---------");
   - In JavaScript, Every Parameter Is Optional => "undefined" If You Didnt Use It
   - "?" Optional Parameter
 */
-function showData(name?:string , age?:number,country?:string){
-    return `${name} - ${age} - ${country}`
+function showData(name?: string, age?: number, country?: string) {
+  return `${name} - ${age} - ${country}`
 }
 console.log(showData())
-console.log(showData("Nasr",29,"Romania"))
+console.log(showData("Nasr", 29, "Romania"))
 
 console.log("---------");
 /*
@@ -83,17 +85,17 @@ console.log("---------");
   - Rest Parameter
   - What About Float => All Is Under Type Number
 */
-function addAll(...nums: number[]): number{
-    let result = 0;
-    // for(let i = 0 ; i < nums.length ; i++){
-    //     result += nums[i];
-    // }
-    nums.forEach(num => {
-        result += num
-    });
-    return result;
+function addAll(...nums: number[]): number {
+  let result = 0;
+  // for(let i = 0 ; i < nums.length ; i++){
+  //     result += nums[i];
+  // }
+  nums.forEach(num => {
+    result += num
+  });
+  return result;
 }
-console.log(addAll(10,20,30,100,10.5,+true));
+console.log(addAll(10, 20, 30, 100, 10.5, +true));
 
 console.log("---------");
 /*
@@ -101,14 +103,14 @@ console.log("---------");
   - Anonymous Function
   - Arrow Function
 */
-const addNumber = function(num1: number , num2: number) : number {
-    return num1 + num2;
+const addNumber = function (num1: number, num2: number): number {
+  return num1 + num2;
 }
 
-console.log(addNumber(10,20));
+console.log(addNumber(10, 20));
 
-const addWithArrow = (num1: number , num2: number) : number => num1 + num2;
+const addWithArrow = (num1: number, num2: number): number => num1 + num2;
 
-console.log(addWithArrow(20,10));
+console.log(addWithArrow(20, 10));
 
 console.log("---------");
